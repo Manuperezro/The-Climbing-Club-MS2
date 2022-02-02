61720cf4092aced0f1e3d4d55391a2035c50b61e
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-02-02 19:36:35 +0000

    Retreats and Courses section implemented with sliding menu

--- assets/css/script/main.js
+++ assets/css/script/main.js
@@ -21,3 +21,23 @@ const navSlide = () => {
     });
 }
 navSlide();
+
+
+//Swiper
+//Initialize Swipper
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
