5c203a396a8a2b2a4233c34b2e9beb43866fedf7
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-02-08 17:30:55 +0000

    Bug rendering the map from google maps API fixed

--- assets/css/script/retreats.js
+++ assets/css/script/retreats.js
@@ -1,15 +1,27 @@
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
+  var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: {
+          lat: 36.92958476555766,
+          lng: -4.798181557169018  
+      }
+  });
+  
+  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  var locations = [
+      { lat: 40.785091, lng: -73.968285 },
+      { lat: 41.084045, lng: -73.874245 },
+      { lat: 40.754932, lng: -73.984016 },  
+      { lat: 50.45487, lng: -4.06066 }
+  ];
+  
+  var markers = locations.map(function(location, i) {
+      return new google.maps.Marker({
+          position: location,
+          label: labels[i % labels.length],
+      });
+  });
+  
+  var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
   }
