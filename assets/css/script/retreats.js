6d6061defe8fbce02c2e3a607f1b333d7cc92da3
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-02-09 18:45:25 +0000

    Map section css style added

--- assets/css/script/retreats.js
+++ assets/css/script/retreats.js
@@ -1,6 +1,6 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 12,
         center: {
             lat: 36.92958476555766,
             lng: -4.798181557169018  
