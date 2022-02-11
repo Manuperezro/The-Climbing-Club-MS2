555262e4ac5eb78d9c31ab1b17eedf60489efd83
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-02-11 17:19:12 +0000

    Mark Clustr bug fixed

--- assets/css/script/retreats.js
+++ assets/css/script/retreats.js
@@ -10,10 +10,8 @@ function initMap() {
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 },  
-        { lat: 50.45487, lng: -4.06066 }
+      { lat: 36.91143523524193, lng:  -4.7670860779893065 }, 
+      { lat: 36.9159644957474,lng: -4.808370637623996 },  
     ];
     
     var markers = locations.map(function(location, i) {
@@ -26,44 +24,3 @@ function initMap() {
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
     }
 
-// import { MarkerClusterer } from "@googlemaps/markerclusterer";
-
-// function initMap() {
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 10,
-//     center: { lat: 36.92958476555766, lng: -4.798181557169018  },
-//   });
-//   const infoWindow = new google.maps.InfoWindow({
-//     content: "",
-//     disableAutoPan: true,
-//   });
-//   // Create an array of alphabetical characters used to label the markers.
-//   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   // Add some markers to the map.
-//   const markers = locations.map((position, i) => {
-//     const label = labels[i % labels.length];
-//     const marker = new google.maps.Marker({
-//       position,
-//       label,
-//     });
-
-//     // markers can only be keyboard focusable when they have click listeners
-//     // open info window when marker is clicked
-//     marker.addListener("click", () => {
-//       infoWindow.setContent(label);
-//       infoWindow.open(map, marker);
-//     });
-//     return marker;
-//   });
-
-//   // Add a marker clusterer to manage the markers.
-//   new MarkerClusterer({ markers, map });
-// }
-
-// const locations = [
-//     { lat: 36.93019907628941, lng: -4.798308033519096 },
-//     { lat: 36.93308065490375, lng: -4.803693908635756 }, 
-//     { lat: 36.93097093838573, lng: -4.8045736730970425 },
-//     { lat: 36.928964080678476, lng:  -4.8010546152518945 }
-// ];
- 
