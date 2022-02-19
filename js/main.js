cde4516d684bb884f580fb4a3951ef66ac4d3d04
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-02-19 20:20:59 +0000

    new code for weather.js . still some  bugs to fix

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 // Animation Navigation Var.
 // Code from CodingLab  Youtube Channel.
 const body = document.querySelector("body"),
-      nav = document.querySelector("nav"),
-      modeToggle = document.querySelector(".dark-light"),
-      searchToggle = document.querySelector(".searchToggle"),
-      sidebarOpen = document.querySelector(".sidebarOpen"),
-      siderbarClose = document.querySelector(".siderbarClose");
+    nav = document.querySelector("nav"),
+    modeToggle = document.querySelector(".dark-light"),
+    searchToggle = document.querySelector(".searchToggle"),
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    siderbarClose = document.querySelector(".siderbarClose");
 
-      let getMode = localStorage.getItem("mode");
-          if(getMode && getMode === "dark-mode"){
-            body.classList.add("dark");
-          }
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+    body.classList.add("dark");
+}
 
 // js code to toggle dark and light mode
-      modeToggle.addEventListener("click" , () =>{
-        modeToggle.classList.toggle("active");
-        body.classList.toggle("dark");
+modeToggle.addEventListener("click", () => {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
 
-        // js code to keep user selected mode even page refresh or file reopen
-        if(!body.classList.contains("dark")){
-            localStorage.setItem("mode" , "light-mode");
-        }else{
-            localStorage.setItem("mode" , "dark-mode");
-        }
-      });
+    // js code to keep user selected mode even page refresh or file reopen
+    if (!body.classList.contains("dark")) {
+        localStorage.setItem("mode", "light-mode");
+    } else {
+        localStorage.setItem("mode", "dark-mode");
+    }
+});
 
 // js code to toggle search box
-        searchToggle.addEventListener("click" , () =>{
-        searchToggle.classList.toggle("active");
-      });
- 
-      
+searchToggle.addEventListener("click", () => {
+    searchToggle.classList.toggle("active");
+});
+
+
 //   js code to toggle sidebar
-sidebarOpen.addEventListener("click" , () =>{
+sidebarOpen.addEventListener("click", () => {
     nav.classList.add("active");
 });
 
-body.addEventListener("click" , e =>{
+body.addEventListener("click", e => {
     let clickedElm = e.target;
 
-    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+    if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
         nav.classList.remove("active");
     }
 });
@@ -50,35 +50,35 @@ body.addEventListener("click" , e =>{
 
 //Initialize owl Carousel
 
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:3
+        1000: {
+            items: 3
         }
     }
 })
 
 
- 
-$(document).ready(function(){
+
+$(document).ready(function () {
     $("#testimonial-slider").owlCarousel({
-        items:1,
-        itemsDesktop:[1000,1],
-        itemsDesktopSmall:[979,1],
-        itemsTablet:[768,1],
-        pagination:false,
-        navigation:true,
-        navigationText:["",""],
-        autoPlay:true
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: false,
+        navigation: true,
+        navigationText: ["", ""],
+        autoPlay: true
     });
 });
 
@@ -112,8 +112,3 @@ $(document).ready(function(){
 //     clearInterval(timer);
 //     timer = null;
 // }
-
-
-
-
-
--- js/weather.js
+++ js/weather.js
@@ -1,19 +1,38 @@
-//fetch data from openweather API
+class Fetch {
+  async getCurrent(input) {
+    const myKey = "cafa79d3ecd466d1aa8ed88778ab568c";
 
-button.addEventListener('click', function(name){
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputText.value+'&appid=cafa79d3ecd466d1aa8ed88778ab568c')
-.then(response => response.json())
-.then(data => {
-  var tempValue = data['main']['temp'];
-  var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
+    //make request to url
 
-  main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
-  input.value ="";
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+    );
 
-})
-// Error Alert 
-.catch(err => alert("Wrong city name!"));
-})
+    const data = await response.json();
+
+    console.log(data);
+
+    return data;
+  }
+}
+
+
+// //fetch data from openweather API
+
+// button.addEventListener('click', function(name){
+//   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputText.value+'&appid=cafa79d3ecd466d1aa8ed88778ab568c')
+// .then(response => response.json())
+// .then(data => {
+//   var tempValue = data['main']['temp'];
+//   var nameValue = data['name'];
+//   var descValue = data['weather'][0]['description'];
+
+//   main.innerHTML = nameValue;
+//   desc.innerHTML = "Desc - "+descValue;
+//   temp.innerHTML = "Temp - "+tempValue;
+//   input.value ="";
+
+// })
+// // Error Alert 
+// .catch(err => alert("Wrong city name!"));
+// })
