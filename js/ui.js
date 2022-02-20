18b655ecc2327bb3a6dfdf205428e34aec2cfc79
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-02-20 10:37:59 +0000

    weather app fixed

--- js/ui.js
+++ js/ui.js
@@ -13,9 +13,10 @@ class UI {
       this.uiContainer.innerHTML = `
           
           <div class="card mx-auto mt-5" style="width: 18rem;">
-              <div class="card-body justify-content-center">
+              <div class="card-body2 justify-content-center">
                   <h5 class="card-title">${data.name}</h5>
                   <h6 class="card-subtitle mb-2 text-muted">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min}</h6>
+                  <h6 class="card-subtitle mb-2 text-muted">Winds: Speed ${data.wind.speed}. Visibility : ${data.visibility}</h6>
                   <p class="card-text ">Weather conditions are described as: ${data.weather[0].description}</p>
                   
               </div>
